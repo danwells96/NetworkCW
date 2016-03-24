e4f9ba718922a29b96a3f461aa969e7eb1e96a91
SnaKIe_nn <ysnakie@hotmail.com>
2016-03-24 21:10:07 +0000

    wl

--- rmi/RMIClient.java
+++ rmi/RMIClient.java
@@ -4,25 +4,21 @@
  */
 package rmi;
 
-import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
-import java.rmi.Remote;
 import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
-import java.rmi.NotBoundException;
-import java.rmi.RemoteException;
-import java.rmi.RMISecurityManager; // deprecated? use SecurityManger instead?
 
 import common.MessageInfo;
+
+import java.io.IOException;
+import java.util.logging.*;
+import java.net.MalformedURLException;
 
 /**
  * @author bandara
  *
  */
 public class RMIClient {
-
 	public static void main(String[] args) {
 
 		RMIServerI iRMIServer = null;
@@ -37,27 +33,29 @@ public class RMIClient {
 		int numMessages = Integer.parseInt(args[1]);
 
 		// TO-DO: Initialise Security Manager
-	
-		if (System.getSecurityManager()==null)
-			System.setSecurityManager(new RMISecurityManager());
+        if (System.getSecurityManager() == null) {
+            System.setSecurityManager(new SecurityManager());
+        }
+
 		// TO-DO: Bind to RMIServer
-		try {
-			String name = "RMIServerI";
-			Registry registry = LocateRegistry.getRegistry(args[0],2000);
-			iRMIServer = (RMIServerI) registry.lookup(name);
+        try {
+            iRMIServer = (RMIServerI) Naming.lookup(urlServer);
+        } catch
+        (NotBoundException|MalformedURLException|RemoteException e) {
+            System.out.println("Exception:" + e.getMessage());
+            System.exit(-1);
+        }
 
 		// TO-DO: Attempt to send messages the specified number of times
-			for (int i=0; i<=numMessages; i++) {
-				MessageInfo msg = new MessageInfo(numMessages, i);
-				iRMIServer.receiveMessage(msg);
-			}
-		}
-		catch (Exception e) {
-			System.out.println(e);
-		}
-		
-
-
-
-	}
+        for (int i = 0; i < numMessages; i++) {
+            MessageInfo msg = new MessageInfo(numMessages, i);
+            try {
+                iRMIServer.receiveMessage(msg);
+            } catch (RemoteException e) {
+                System.out.println("Exception:" + e.getMessage() +
+                        " -- message " + Integer.toString(i) +
+                        " unsent");
+            }
+        }
+    }
 }
--- rmi/RMIServer.java
+++ rmi/RMIServer.java
@@ -10,117 +10,150 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.Arrays;
-import java.rmi.RMISecurityManager;
-import java.rmi.registry.*;
-import java.net.InetAddress;
-import common.*;
-import java.rmi.Naming;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.RemoteException;
-import java.rmi.server.UnicastRemoteObject;
-import java.util.Arrays;
 
 import common.*;
+
+import java.rmi.registry.Registry;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.io.IOException;
+import java.util.logging.*;
 
 /**
  * @author bandara
  *
  */
 public class RMIServer extends UnicastRemoteObject implements RMIServerI {
+    private final static Logger logger =
+        Logger.getLogger(RMIServer.class.getName());
+    private static FileHandler fh = null;
 
 	private int totalMessages = -1;
 	private int[] receivedMessages;
+    private int cnt_msg_recv = 0;
+
+    private static int registry_port = 1099;
 
 	public RMIServer() throws RemoteException {
-		super();
 	}
 
 	public void receiveMessage(MessageInfo msg) throws RemoteException {
-		// TO-DO: On receipt of first message, initialise the receive buffer
-		totalMessages = msg.totalMessages;
 
-		if (totalMessages == -1) {
-			receivedMessages = new int[totalMessages];
-			for (int i=0; i<totalMessages; i++) {
-				receivedMessages[i] = 0;
-			}
-		}
-		
+		// TO-DO: On receipt of first message, initialise the receive buffer
+        if (totalMessages == -1) {
+            totalMessages = msg.totalMessages;
+            cnt_msg_recv = 0;
+            receivedMessages = new int[totalMessages];
+        }
+
 		// TO-DO: Log receipt of the message
-		receivedMessages[msg.messageNum-1] = 1;
-		System.out.println("Message " + msg.messageNum + " was received");
-		
+        logger.info("Message " + String.format("%4d", msg.messageNum)
+                + " received");
+        receivedMessages[msg.messageNum] = 1;
+        cnt_msg_recv++;
 
 		// TO-DO: If this is the last expected message, then identify
 		//        any missing messages
-		if(msg.messageNum == totalMessages) {
-			System.out.print("The following messages are missed: ");
-	
-			for(int i = 0; i < totalMessages; i++) {
-				if(receivedMessages[i] == 0) {
-					System.out.print(i + ", ");
-				}
-			}
-			totalMessages = -1;
-		}
+        if (cnt_msg_recv == totalMessages) {
+            logSummary();
+
+            // unbind
+            try {
+                Naming.unbind("rmi://localhost/RMIServer");
+                unexportObject(this, true);
+            } catch (Exception e) {}
+        }
 	}
 
+    private void logSummary() {
+        logger.info("");
+        logger.info("================ START OF SUMMARY ================");
+        if (totalMessages == -1) {
+            logger.warning("No messages have been received");
+        }
+        else {
+            logger.info("n_total_messages    = " +
+                    Integer.toString(totalMessages));
+            logger.info("n_messages_received = " +
+                    Integer.toString(cnt_msg_recv));
+
+            if (cnt_msg_recv == totalMessages) {
+                logger.info("All messages successfully received!");
+            }
+            else {
+                String msg_missed = new String();
+                for (int i = 0; i < totalMessages; i++) {
+                    if (receivedMessages[i] == 0) {
+                        if (msg_missed.length() != 0) {
+                            msg_missed += ", ";
+                        }
+                        msg_missed += Integer.toString(i);
+                    }
+                }
+                logger.info("messages_missed     = " + msg_missed);
+            }
+        }
+        logger.info("================= END OF SUMMARY =================");
+    }
+
+    private static void initLogFile() {
+        try {
+            fh = new FileHandler("rmiserver.log");
+        } catch (IOException e) {
+            System.out.println("IO:" + e.getMessage());
+        }
+        logger.addHandler(fh);
+        SimpleFormatter formatter = new SimpleFormatter();
+        fh.setFormatter(formatter);
+    }
 
 	public static void main(String[] args) {
 
 		RMIServer rmis = null;
-		try {
-			//Get IP address and print it to the screen
-			InetAddress addr = InetAddress.getLocalHost();
-			String ipAddress = addr.getHostAddress();
-			System.out.println("Local Host = " + addr);
-			System.out.println("Host = " + ipAddress);
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
-		
-		try {
-			//Initialise RMIServer and set the RMISecurityManager
-			System.setSecurityManager(new RMISecurityManager());
-			rmis = new RMIServer();
-			//Rebind the RMI Server
-			String url = new String("RMIServerI");
-			rebindServer(url, rmis);
-		}
-		catch (RemoteException e)
-		{
-			System.out.println("Remote Exception");
-		}
-	}
 
-	protected static void rebindServer(String serverURL, RMIServer server) {
+        initLogFile();
+        // TO-DO: Initialise Security Manager
+        if (System.getSecurityManager() == null) {
+            System.setSecurityManager(new SecurityManager());
+        }
 
-		// TO-DO:
-		try {
-			Registry registry;
-			try {
-				// locate registry and create port at 1099
-				registry = LocateRegistry.createRegistry(2000);
-			}
-			catch (RemoteException e) {
-				registry = LocateRegistry.getRegistry();
-			}
-			// bind
-			registry.rebind(serverURL, server);
-			System.out.println(registry.list());
-		}
-		catch (Exception e) {
-			System.out.println("Error in " + e.getMessage());
-		}
-		
-		// Start / find the registry (hint use LocateRegistry.createRegistry(...)
-		// If we *know* the registry is running we could skip this (eg run rmiregistry in the start script)
+        // TO-DO: Instantiate the server class
+        try {
+            rmis = new RMIServer();
 
-		// TO-DO:
-		// Now rebind the server to the registry (rebind replaces any existing servers bound to the serverURL)
-		// Note - Registry.rebind (as returned by createRegistry / getRegistry) does something similar but
-		// expects different things from the URL field.
-	}
+        } catch (RemoteException e) {
+            logger.severe("Exception:" + e.getMessage());
+            System.exit(-1);
+        }
+
+        String hostname;
+        try {
+            hostname = InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+            hostname = new String();
+        }
+        logger.info("RMI server ready, running on " + hostname);
+
+        // TO-DO: Bind to RMI registry
+        String url = new String("rmi://localhost/RMIServer");
+        rebindServer(url, rmis);
+    }
+
+    protected static void rebindServer(String serverURL, RMIServer server) {
+
+        // TO-DO:
+        // Start / find the registry (hint use LocateRegistry.createRegistry(...)
+        // If we *know* the registry is running we could skip this (eg run rmiregistry in the start script)
+        // NOTE: rmiregistry is run the the start script
+
+        // TO-DO:
+        // Now rebind the server to the registry (rebind replaces any existing servers bound to the serverURL)
+        // Note - Registry.rebind (as returned by createRegistry / getRegistry) does something similar but
+        // expects different things from the URL field.
+        try {
+            Naming.rebind(serverURL, server);
+        } catch (RemoteException|MalformedURLException e) {
+            logger.severe("Exception:" + e.getMessage());
+        }
+    }
 }
